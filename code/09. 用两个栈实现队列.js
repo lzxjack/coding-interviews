70ea68beef461f0ddfe5f4bbcda4a4865ed6a145
Jack <965555169@qq.com>
2021-10-23 21:29:53 +0800

    update

--- code/09. 用两个栈实现队列.js
+++ code/09. 用两个栈实现队列.js
@@ -1,23 +1,23 @@
-var CQueue = function () {
-    this.stackA = [];
-    this.stackB = [];
-};
-
-CQueue.prototype.appendTail = function (value) {
-    this.stackA.push(value);
-};
-
-CQueue.prototype.deleteHead = function () {
-    if (this.stackB.length) {
-        return this.stackB.pop();
-    } else {
-        while (this.stackA.length) {
-            this.stackB.push(this.stackA.pop());
-        }
-        if (!this.stackB.length) {
-            return -1;
-        } else {
+class CQueue {
+    constructor() {
+        this.stackA = [];
+        this.stackB = [];
+    }
+    appendTail(value) {
+        this.stackA.push(value);
+    }
+    deleteHead() {
+        if (this.stackB.length) {
             return this.stackB.pop();
+        } else {
+            while (this.stackA.length) {
+                this.stackB.push(this.stackA.pop());
+            }
+            if (!this.stackB.length) {
+                return -1;
+            } else {
+                return this.stackB.pop();
+            }
         }
     }
-};
+}
