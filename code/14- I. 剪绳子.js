617c6e7eb25917cdf51c759c4088fedefc71c13e
Jack <965555169@qq.com>
2021-10-26 19:27:24 +0800

    update

--- code/14- I. 剪绳子.js
+++ code/14- I. 剪绳子.js
@@ -1,8 +1,12 @@
 const cuttingRope = n => {
     const dp = [null, null, 1];
     for (let i = 3; i <= n; i++) {
+        // 当前dp[i]初始化为0
         dp[i] = 0;
         for (let j = 1; j <= i - 1; j++) {
+            // 因为j在遍历的过程中，会算出很多dp[i]，取最大的
+            // (i - j) * j表示拆分成2个数
+            // dp[i - j] * j表示拆分成2个及以上的数
             dp[i] = Math.max(dp[i], (i - j) * j, dp[i - j] * j);
         }
     }
--- code/14- II. 剪绳子 II.js
+++ code/14- II. 剪绳子 II.js
@@ -1,14 +1,17 @@
 // 贪心：每次拆成n个3，如果剩下是4，则保留4，然后相乘
 
 const cuttingRope = n => {
+    // 特殊情况处理
     const arr = [null, null, 1, 2, 4];
     if (n <= 4) return arr[n];
     const mod = 1000000007;
     let res = 1;
     while (n > 4) {
+        // 每次减掉3
         res = (res * 3) % mod;
         n -= 3;
     }
+    // 最后剩下一段小于等于4的长度
     res *= n;
     return res % mod;
 };
