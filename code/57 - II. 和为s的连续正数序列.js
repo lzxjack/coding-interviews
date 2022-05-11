bd795ca29c9560f8cde581be0b1bc062f074d2aa
飞鸟 <965555169@qq.com>
2022-05-11 10:31:40 +0800

    update

--- code/57 - II. 和为s的连续正数序列.js
+++ code/57 - II. 和为s的连续正数序列.js
@@ -1,23 +1,19 @@
 const findContinuousSequence = target => {
-    let [left, right] = [1, 1];
-    let sum = 0;
-    const res = [];
-    while (left <= target >> 1) {
-        // sum=(首项+尾项)*项数/2
-        sum = ((left + right) * (right - left + 1)) >> 1;
-        if (sum < target) {
-            right++;
-        } else if (sum > target) {
-            left++;
-        } else {
-            const res_item = [];
-            // 将[left,right]中的数都输出
-            for (let i = left; i <= right; i++) {
-                res_item.push(i);
-            }
-            res.push(res_item);
-            left++;
-        }
+  const res = [];
+  const window = [1, 2];
+  let sum = 3;
+
+  while (window[0] <= target >> 1) {
+    if (sum < target) {
+      const num = window[window.length - 1] + 1;
+      sum += num;
+      window.push(num);
+    } else if (sum > target) {
+      sum -= window.shift();
+    } else {
+      res.push([...window]);
+      sum -= window.shift();
     }
-    return res;
+  }
+  return res;
 };
